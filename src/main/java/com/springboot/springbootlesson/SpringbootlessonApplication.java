1e2be6c00a7bae9a4580c4733891b6fe6069cfca
gemdoq <gemdoq@gmail.com>
2022-10-27 22:22:38 +0900

    SpringBoot Lesson Print Hello World

---
+++ src/main/java/com/springboot/springbootlesson/SpringbootlessonApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootlesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootlessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootlessonApplication.class, args);
+	}
+
+}
