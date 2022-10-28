8250aa90229ff0acae44a3ef1afbc10451c429bf
gemdoq <gemdoq@gmail.com>
2022-10-28 13:58:16 +0900

    SpringBoot Lesson created User in domain, UserDao in dao

---
+++ src/main/java/com/springboot/springbootlesson/dao/UserDao.java
@@ -0,0 +1,53 @@
+package com.springboot.springbootlesson.dao;
+
+import com.springboot.springbootlesson.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserDao {
+
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void deleteAll() throws ClassNotFoundException, SQLException {
+        String sql = "delete from users";
+        jdbcTemplate.update(sql);
+    }
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        String sql = "insert into users(id,name,password) values(?,?,?);";
+        jdbcTemplate.update(sql, user.getId(), user.getName(), user.getPassword());
+    }
+
+    public int getCount() throws SQLException, ClassNotFoundException {
+        String sql = "select count(*) from users";
+        return jdbcTemplate.queryForObject(sql, Integer.class);
+    }
+
+    RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+        }
+    };
+
+    public User findById(String id) throws ClassNotFoundException, SQLException {
+        String sql = "select * from users where id = ?";
+        return jdbcTemplate.queryForObject(sql, rowMapper, id);
+    }
+
+    public List<User> getAll() {
+        String sql = "select * from users order by id";
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+}
