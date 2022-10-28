3eec2fb546303d1f2edb3baa2648bfe967ee4c50
gemdoq <gemdoq@gmail.com>
2022-10-28 12:32:46 +0900

    SpringBoot Lesson add logback(logging library)

--- src/main/java/com/springboot/springbootlesson/controller/GetController.java
+++ src/main/java/com/springboot/springbootlesson/controller/GetController.java
@@ -1,29 +1,34 @@
 package com.springboot.springbootlesson.controller;
 
 import com.springboot.springbootlesson.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
 
     //http://localhost:8080/api/v1/get-api/hello
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getHello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
 
     //http://localhost:8080/api/v1/get-api/hello
     @GetMapping("/name")
     public String getName() {
+        log.info("getName으로 요청이 들어왔습니다.");
         return "This is name";
     }
 
     //http://localhost:8080/api/v1/get-api/variable1/{String string}
     @GetMapping("/variable1/{variable}")
     public String getVariable(@PathVariable String variable) {
+//        log.info("getVariable1으로 요청이 들어왔습니다. variable:{ }", variable);
         return variable;
     }
 
