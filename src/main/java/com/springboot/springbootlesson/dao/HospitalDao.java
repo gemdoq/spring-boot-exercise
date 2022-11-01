50c40b7c8df85ed4b5e2a364938f61e2154e9770
gemdoq <gemdoq@gmail.com>
2022-11-01 13:43:10 +0900

    SpringBoot Lesson HospitalDao add method

--- src/main/java/com/springboot/springbootlesson/dao/HospitalDao.java
+++ src/main/java/com/springboot/springbootlesson/dao/HospitalDao.java
@@ -1,5 +1,19 @@
 package com.springboot.springbootlesson.dao;
 
+import com.springboot.springbootlesson.domain.Hospital;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.SQLException;
+
 public class HospitalDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public HospitalDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    public void add(Hospital hospital) throws ClassNotFoundException, SQLException {
+        String sql = "INSERT INTO `likeliondb`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+        jdbcTemplate.update(sql, hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(), hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(), hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(), hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(), hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(), hospital.getTotalAreaSize() );
+    }
 
 }
