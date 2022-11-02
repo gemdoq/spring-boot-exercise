b8d7110fb22c896a322d057afdc4b46189069e44
gemdoq <gemdoq@gmail.com>
2022-11-02 14:23:34 +0900

    SpringBoot Lesson modified HospitalController

--- src/main/java/com/springboot/springbootlesson/controller/HospitalController.java
+++ src/main/java/com/springboot/springbootlesson/controller/HospitalController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/hospitals")
+@RequestMapping("/api/v1/hospital")
 public class HospitalController {
     private final HospitalDao hospitalDao;
 
